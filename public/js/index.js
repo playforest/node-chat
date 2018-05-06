7a65922e39897d08c264e8b5ea83dc54360bf43a
idhahbi <idhahbi@gmail.com>
2018-05-06 21:20:53 +1000

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
         
 socket.on('connect', function() {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'imi@dr.com',
-        text: 'yo jordan, wats up?'
-    });
 });
 
 socket.on('disconnect', function() {
