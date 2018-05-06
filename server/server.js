2c3addccee2e24ab3b634c936bdd1b02d43cfcf7
idhahbi <idhahbi@gmail.com>
2018-05-06 22:06:50 +1000

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,18 +14,30 @@ app.use('/', express.static(publicPath))
 io.on('connection', function(socket) {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        // io.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
-        socket.broadcast.emit('newMessage', {
+        io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     })
 
     socket.on('disconnect', function() {
